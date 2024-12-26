a8932dcc2c3e2f1583c5c94b214e0694e534a9bd
RendiMangkuRondo <rendisaputrasaputra389@gmail.com>
2024-12-26 14:00:13 +0700

    first commit

---
+++ all/security/adiwConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'approval': {
+    'num': '6287833288249',
+    'text': "oke",
+    'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
+    'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
+  },
+  'creatorScript': '6287833288249',
+  'filePath': "./all/approval",
+  'checkFilePath': "../setting/osaragi.js",
+  'codeToDetect': 'main();'
+};
